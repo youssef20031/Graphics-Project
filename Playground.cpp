0ae6a7c95cbf017d9239931b5519e1eaa51c6da1
Ziad Raafat <zrafat65@gmail.com>
2024-11-24 14:38:17 +0200

    Put camera in separate files

--- Playground.cpp
+++ Playground.cpp
@@ -37,68 +37,6 @@ ViewMode viewMode = THIRD_PERSON;
 
 
 
-// Camera class constructor and methods
-
-Camera::Camera(float eyeX, float eyeY, float eyeZ, float centerX, float centerY, float centerZ, float upX, float upY, float upZ) {
-	eye = Vector3f(eyeX, eyeY, eyeZ);
-	center = Vector3f(centerX, centerY, centerZ);
-	up = Vector3f(upX, upY, upZ);
-}
-
-// Move camera along the X axis
-void Camera::moveX(float d) {
-	Vector3f right = up.cross(center - eye).unit();
-	eye = eye + right * d;
-	center = center + right * d;
-}
-
-// Move camera along the Y axis
-void Camera::moveY(float d) {
-	eye = eye + up.unit() * d;
-	center = center + up.unit() * d;
-}
-
-// Move camera along the Z axis
-void Camera::moveZ(float d) {
-	Vector3f view = (center - eye).unit();
-	eye = eye + view * d;
-	center = center + view * d;
-}
-
-// Rotate camera around the X axis
-void Camera::rotateX(float a) {
-	Vector3f view = (center - eye).unit();
-	Vector3f right = up.cross(view).unit();
-	view = view * cos(DEG2RAD(a)) + up * sin(DEG2RAD(a));
-	up = view.cross(right);
-	center = eye + view;
-}
-
-// Rotate camera around the Y axis
-void Camera::rotateY(float a) {
-	Vector3f view = (center - eye).unit();
-	Vector3f right = up.cross(view).unit();
-	view = view * cos(DEG2RAD(a)) + right * sin(DEG2RAD(a));
-	right = view.cross(up);
-	center = eye + view;
-}
-
-// Set camera position and orientation
-void Camera::look() {
-	gluLookAt(
-		eye.x, eye.y, eye.z,
-		center.x, center.y, center.z,
-		up.x, up.y, up.z
-	);
-}
-
-void Camera::setView(float eyeX, float eyeY, float eyeZ, float centerX, float centerY, float centerZ, float upX, float upY, float upZ) {
-	eye = Vector3f(eyeX, eyeY, eyeZ);
-	center = Vector3f(centerX, centerY, centerZ);
-	up = Vector3f(upX, upY, upZ);
-}
-
-Camera camera;
 
 void drawCuboid(double xStart, double xEnd, double yStart, double yEnd, double zStart, double zEnd) {
 	glPushMatrix();
