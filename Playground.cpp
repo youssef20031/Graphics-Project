0066060cb63b51fece7a8b5e36bce37c59a0d42b
Ziad Raafat <zrafat65@gmail.com>
2024-11-24 18:32:17 +0200

    removed extra spacing

--- Playground.cpp
+++ Playground.cpp
@@ -1,23 +1,15 @@
 #include "Playground.h"
 #include <cmath> 
-/////////////////////////////// Display ////////////////////////////////
 void Display() {
 	setupCamera();
 	setupLights();
-
 	glutFullScreen();
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 	displayCameraCoords();
-
 	drawAxis();
-
 	glColor3f(0.2f, 0.7f, 0.8f);
 	drawCuboid(0.0f, 3.0f, 0.0f, 0.1f, 0.0f, 3.0f); // ground
-
-	drawPlayer();
-	 
+	drawPlayer(); 
 	glFlush();
 	glutSwapBuffers();
 }
