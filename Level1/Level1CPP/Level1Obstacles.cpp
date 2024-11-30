46157a474154e30ce2ed2ec8fc4c21f3920a04d9
Ziad Raafat <zrafat65@gmail.com>
2024-12-01 01:21:31 +0200

    THE DRAGON HAS ARRIVED

--- Level1/Level1CPP/Level1Obstacles.cpp
+++ Level1/Level1CPP/Level1Obstacles.cpp
@@ -353,6 +353,10 @@ void LoadAssetsL1()
 
     //dragon
     dragon.Load("Models/dragon/Drachen_1.0_3ds.3ds");
+    dragon.pos.x = -90.51;
+    dragon.pos.y = 0.2;
+    dragon.pos.z = 70.12;
+    dragon.rot.y = 110;
 
     
     
